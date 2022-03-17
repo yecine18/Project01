fbd3c8a777a824c03cd0367b23039d7229926d22
Khaled Bribri <94048267+Khaled-Bribri@users.noreply.github.com>
2022-03-17 16:41:45 +0100

    J'ai ajouté ma partie dans devkhaled

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project-01</title>
+    <link rel="stylesheet" href="khaled.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans" >
+    <link rel="stylesheet" href="https://unpkg.com/bulma@0.7.5/css/bulma.min.css">
+    
+</head>
+
+<body>
+
+
+<div>
+	<div class="column is-12">
+		<section class="hero is-info is-bold is-small">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title">Hello /Firstname/ /Lastname/ from /Campus/, this is your message : </h1>
+					<h2 class="subtitle">/Message/</h2>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+
+
+<footer>
+    © 2022 wilders
+</footer>
+</body>
+
+</html>
